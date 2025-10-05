aa976c4e808f48fdd335bc6aaa9e4d892e2ef88c
ys-yeom <yuseo3489@gmail.com>
2025-10-05 18:04:25 +0900

    작업 내용

---
+++ src/main/java/com/trade/ProjectTrade/ProjectTradeApplication.java
@@ -0,0 +1,13 @@
+package com.trade.ProjectTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTradeApplication.class, args);
+	}
+
+}
